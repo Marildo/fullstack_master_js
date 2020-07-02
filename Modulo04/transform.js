1120d8b2e519157842a72aec80a6c472b6603372
unknown <marildocesar@hotmail.com>
2020-07-01 21:18:01 -0300

    Finalizando transformacao

--- Modulo04/transform.js
+++ Modulo04/transform.js
@@ -36,6 +36,18 @@ const dados = [
     saldoCaixa: 451.28
   },
   {
+    title: 'Pioneiros',
+    dataHistorico: '1593140400000',
+    saldoAtivos: 13,
+    saldoCaixa: 3
+  },
+  {
+    title: 'Hope',
+    dataHistorico: '1593140400000',
+    saldoAtivos: 0.17,
+    saldoCaixa: 451.28
+  },
+  {
     title: 'Hope',
     dataHistorico: '1593399600000',
     saldoAtivos: 0.79,
@@ -48,11 +60,17 @@ const dados = [
     saldoCaixa: 1387.4
   },
   {
-    title: 'Hope',
-    dataHistorico: '1593140400000',
-    saldoAtivos: -4859.21,
-    saldoCaixa: 5129.3
-  }
+    title: 'Speed',
+    dataHistorico: '1593399600000',
+    saldoAtivos: 0.79,
+    saldoCaixa: 5240.21
+  },
+  {
+    title: 'Tela',
+    dataHistorico: '1593399600000',
+    saldoAtivos: 0.78,
+    saldoCaixa: 1387.4
+  },
 ]
 
 /*rows: [
@@ -62,7 +80,7 @@ const dados = [
   ]  */
 
 const getData = (item) => {
-  item['data'] = new Date(parseInt(item.dataHistorico)).toLocaleDateString()
+  item['data'] = new Date(parseInt(item.dataHistorico)).toLocaleDateString('pt-BR', { year: 'numeric', month: '2-digit', day: '2-digit' })
   delete item.dataHistorico
   return item
 }
@@ -80,18 +98,34 @@ const somaSaldo = (item) => {
   return item
 }
 
+const keys = Array.from(new Set(dados.map((i) => i.title)))
+keys.unshift('data')
+
 const fase1 = dados.map(getData)
 const fase2 = fase1.map(criarTitulos)
-
 const fase3 = fase2.map(somaSaldo)
-fase3.sort()
-console.log(fase3)
+//console.log(fase3)
+
 
 let datas = Array.from(new Set(fase3.map((i) => i.data)))
+datas.sort()
 
-console.log(datas)
-
+let fase4 = []
 for (let i = 0; i < datas.length; i++) {
-  const values = fase3.filter((i) => i.data === datas[i])
-  console.log(values)
+  const values = fase3.filter(item => item.data === datas[i])
+ fase4.push(values)
 }
+
+const fase5 = []
+for (const value of fase4) {
+  const obj = {}
+  for (const v of value) {
+    for (const key of keys) {
+      if(v[key])
+        obj[key] = v[key]
+    }
+  }
+  fase5.push(obj)
+}
+
+console.log(fase5)
